323e88ba17c9ec66f6b11ecd900c5416ca2c06f0
Azik-Mazik <105788061+Azik-Mazik@users.noreply.github.com>
2022-05-24 19:13:26 +0700

    Update Bot.py

--- 1.0 beta/Bot.py
+++ 1.0 beta/Bot.py
@@ -17,7 +17,7 @@ async def cmds(inter):
     
 @client.command(aliases=['привет', 'ку', 'шалом', 'хай', 'салам', 'Привет', 'Ку', 'Шалом', 'Хай', 'Салам', 'Hello'])
 async def hello(ctx):
-    await ctx.reply("И тебе привет!")
+    await ctx.reply("САЛАМАЛЮКУМ БРО")
 
 @client.command(aliases=['help'])
 async def hello1(ctx):
@@ -28,7 +28,7 @@ async def hello1(ctx):
 async def hello2(ctx):
     emb = disnake.Embed(title="Обо мне", description="\nПросто бот. Создан **@Bad Boy#4708** 05.05.2022. В даный момент нахдится на бета тесте на сервере AlexGyver community\n\nВерсия 1.0 beta", colour=disnake.Colour.red())
     view = disnake.ui.View()
-    url = disnake.ui.Button(label='Мой github', url='https://github.com/Ivan1240/Discord_bot/')
+    url = disnake.ui.Button(label='Лава и Артем ночью без родителей', url='https://github.com/Ivan1240/Discord_bot/')
     view.add_item(item=url)
     await ctx.reply(embed=emb, view=view)
     
